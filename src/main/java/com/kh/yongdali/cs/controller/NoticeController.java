e78b23c6625759f9390821a04b48b37b96e95f0f
Tamhee_Lee <tamhee0110@naver.com>
2020-04-06 19:15:33 +0900

    0406 탐희 재커밋

--- src/main/java/com/kh/yongdali/cs/controller/NoticeController.java
+++ src/main/java/com/kh/yongdali/cs/controller/NoticeController.java
@@ -4,7 +4,8 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
