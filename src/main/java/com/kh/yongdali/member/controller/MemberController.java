0c68e12ee507af1e2d4611505ac7979dd44883a1
raejin <raejin@192.168.120.13>
2020-04-09 01:29:00 +0900

    insertSampleMembers(MemberController/service/dao/mapper/test)

--- src/main/java/com/kh/yongdali/member/controller/MemberController.java
+++ src/main/java/com/kh/yongdali/member/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.kh.yongdali.member.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,6 +28,9 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService mService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
 	
 	private Logger logger = LoggerFactory.getLogger(MemberController.class);
 	
@@ -87,26 +92,47 @@ public class MemberController {
 	}
 	
 	@RequestMapping("insertSample.me")
-	public String insertSample() {
-		String sDate1 = "2020-03-10";
+	public String insertSample(Model model) {
+		ArrayList<Member> mList = new ArrayList<Member>();
+		String[] sDate = new String[4];
+		long[] milliesDate = new long[4];
+		Date[] date = new Date[4];
+		String[] encPwdArr = new String[4];
 		
-		long milliesDate1 = 0;
+		mList.add(new Member("admin@naver.com", "admin", "유승제", "010-1111-1111", "관리자", "정상")); 
+		mList.add(new Member("mem01@naver.com", "mem01", "홍멤버", "010-2222-2222", "일반", "정상"));
+		mList.add(new Member("biz01@naver.com", "biz01", "김거상", "010-3333-3333", "사업자", "정상"));
+		mList.add(new Member("biz02@naver.com", "biz01", "최거상", "010-4444-4444", "사업자", "정상"));
+		
+		sDate[0] = "2020-03-10";
+		sDate[1] = "2020-03-18";
+		sDate[2] = "2020-03-18";
+		sDate[3] = "2020-03-19";
 		
 		try {
-			Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(sDate1);
-			milliesDate1 = date1.getTime();
-						
+			for(int i = 0; i < sDate.length ; i++) {
+				date[i] = new SimpleDateFormat("yyyy-MM-dd").parse(sDate[i]);
+				milliesDate[i] = date[i].getTime();
+				mList.get(i).setEnrollDate(new java.sql.Date(milliesDate[i]));
+				logger.debug(mList.get(i).toString());
+				
+				encPwdArr[i] = bcryptPasswordEncoder.encode(mList.get(i).getPwd());
+				mList.get(i).setPwd(encPwdArr[i]);
+				logger.debug(mList.get(i).toString()+"\n");
+			}
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			model.addAttribute("msg", e.getMessage());
+			return "common/errorPage";
 		}
 		
-		Member m1 = new Member("admin@naver.com", "admin", "유승제", "010-1111-1111", "관리자", "정상", new java.sql.Date(milliesDate1));		
+		int result = mService.insertSampleMembers(mList);
 		
-		
-		
-		
-		return null;
+		if(result > 3) {
+			return "redirect:home.do";
+		}else {
+			model.addAttribute("msg", "샘플데이터 입력 실패!");
+			return "common/errorPage";
+		}
 	}
 	
 }
--- src/main/java/com/kh/yongdali/member/model/vo/Test.java
+++ src/main/java/com/kh/yongdali/member/model/vo/Test.java
@@ -1,76 +1,76 @@
-package com.kh.yongdali.member.model.vo;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.kh.yongdali.member.model.service.MemberService;
-import com.kh.yongdali.member.model.service.MemberServiceImpl;
-
-
-public class Test {
-
-	BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
-	MemberService mService = new MemberServiceImpl();
-	
-	public static void main(String[] args) {
-		new Test().test();
-	}
-	
-	public void test() {
-		String[] sDate = new String[4];
-		sDate[0] = "2020-03-10";
-		sDate[1] = "2020-03-18";
-		sDate[2] = "2020-03-18";
-		sDate[3] = "2020-03-19";
-		
-		Member m1 = new Member("admin@naver.com", "admin", "유승제", "010-1111-1111", "관리자", "정상");
-		
-		
-		long milliesDate1 = 0;
-		
-		try {
-			Date[] date = new Date[4];
-			for(int i = 0; i <= sDate.length ; i++) {
-				Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(sDate[i]);
-				
-			}
-//			Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(sDate1);
-//			milliesDate1 = date1.getTime();
-			
-			
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-//		Member m1 = new Member("admin@naver.com", "admin", "유승제", "010-1111-1111", "관리자", "정상", new java.sql.Date(milliesDate1));
-//		Member m2 = new Member("admin@naver.com", "admin", "유승제", "010-1111-1111", "관리자", "정상", new java.sql.Date(milliesDate1));
-		
-		System.out.println(bcryptPasswordEncoder.encode(m1.getPwd()));
-		
-		String encPwd = bcryptPasswordEncoder.encode(m1.getPwd());
-		
-		m1.setPwd(encPwd);
-		
-		System.out.println(m1);
-		
-		ArrayList<Member> mList = new ArrayList<Member>();
-		
-		mList.add(m1);
-//		mList.add(m2);
-		
-		System.out.println(mList);
-	}
-}
-//	SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-//    String renameFileName = "N"+sdf.format(new java.sql.Date(System.currentTimeMillis()))+"."+
-//                      originFileName.substring(originFileName.lastIndexOf(".")+1);
-
-    
+//package com.kh.yongdali.member.model.vo;
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.ArrayList;
+//import java.util.Date;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//import com.kh.yongdali.member.model.service.MemberService;
+//import com.kh.yongdali.member.model.service.MemberServiceImpl;
+//
+//
+//
+//public class Test {
+//
+//	BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+//	MemberService mService = new MemberServiceImpl();
+//	private Logger logger = LoggerFactory.getLogger(Test.class);
+//	
+//	public static void main(String[] args) {
+//		new Test().test();
+//	}
+//	
+//	public void test() {
+//		ArrayList<Member> mList = new ArrayList<Member>();
+//		String[] sDate = new String[4];
+//		long[] milliesDate = new long[4];
+//		Date[] date = new Date[4];
+//		String[] encPwdArr = new String[4];
+//		
+//		mList.add(new Member("admin@naver.com", "admin", "유승제", "010-1111-1111", "관리자", "정상")); 
+//		mList.add(new Member("mem01@naver.com", "mem01", "홍멤버", "010-2222-2222", "일반", "정상"));
+//		mList.add(new Member("biz01@naver.com", "biz01", "김거상", "010-3333-3333", "사업자", "정상"));
+//		mList.add(new Member("biz02@naver.com", "biz01", "최거상", "010-4444-4444", "사업자", "정상"));
+//		
+//		sDate[0] = "2020-03-10";
+//		sDate[1] = "2020-03-18";
+//		sDate[2] = "2020-03-18";
+//		sDate[3] = "2020-03-19";
+//		
+//		try {
+//			for(int i = 0; i < sDate.length ; i++) {
+//				date[i] = new SimpleDateFormat("yyyy-MM-dd").parse(sDate[i]);
+//				milliesDate[i] = date[i].getTime();
+//				mList.get(i).setEnrollDate(new java.sql.Date(milliesDate[i]));
+//				logger.debug(mList.get(i).toString());
+//				
+//				encPwdArr[i] = bcryptPasswordEncoder.encode(mList.get(i).getPwd());
+//				mList.get(i).setPwd(encPwdArr[i]);
+//				logger.debug(mList.get(i).toString()+"\n");
+//			}
+//		} catch (ParseException e) {
+////			model.addAttribute("msg", e.getMessage());
+////			return "common/errorPage";
+//			e.printStackTrace();
+//		}
+//		
+//		int result = mService.insertSampleMembers(mList);
+//		
+////		if(result > 3) {
+////			return "redirect:home.do";
+////		}else {
+////			model.addAttribute("msg", "샘플데이터 입력 실패!");
+////			return "common/errorPage";
+////		}
+//	}
+//}
+//
+//
+//    
 
 
 
