9063b15c682fa57766633eedc7883464fbe46fb3
wontae <tashuren@naver.com>
2020-04-16 19:05:52 +0900

    my reservation cancel update

--- src/main/java/com/kh/yongdali/driver/model/service/DriverService.java
+++ src/main/java/com/kh/yongdali/driver/model/service/DriverService.java
@@ -2,18 +2,25 @@ package com.kh.yongdali.driver.model.service;
 
 import java.util.ArrayList;
 
+import com.kh.yongdali.common.PageInfo;
 import com.kh.yongdali.reservation.model.vo.Reservation;
 
 public interface DriverService {
 
 	ArrayList<Reservation> mibaechar(String mNO);
 
-	ArrayList<Reservation> myBaechar(int dId);
+	ArrayList<Reservation> myBaechar(String mNo);
 
 	ArrayList<Reservation> driverCal(String mNo);
 
 	ArrayList<Reservation> driverModal(String rNo);
 
 	int Deal(Reservation aa);
+
+	int getListCount(String mNo);
+
+	ArrayList<Reservation> selectList(String mNo,PageInfo pi);
+
+	int cancel(Reservation aa);
 	
 }
